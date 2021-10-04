e4f693b146bf6d88273ba76d3043b427b398b376
masaki.ikeda <masaki.ikeda@oro.com>
2021-10-04 14:01:27 +0900

    Rewriting by Typescript and react.

---
+++ src/bind_polyfill.ts
@@ -0,0 +1,11 @@
+Function.prototype.bind =
+	Function.prototype.bind ||
+	function (target) {
+		const self = this;
+		return function (args) {
+			if (!(args instanceof Array)) {
+				args = [args];
+			}
+			self.apply(target, args);
+		};
+	};
